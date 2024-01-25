3452012b4958ec16afd0ba7b08834b25255a21e8
Karthikeya.Gundumogula <57733888+KarthikeyaGundumogula@users.noreply.github.com>
2024-01-25 14:57:10 +0000

    worked on Assets contract and wrote its tests

--- src/Assets.sol
+++ src/Assets.sol
@@ -1,12 +1,92 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import {console} from "forge-std/Test.sol";
 
-contract Assets is ERC1155 {
-    
+contract Assets is ERC1155URIStorage, ERC1155Holder {
+    error Assets_InvalidTokenID(uint8 tokenID);
+    error Assets_NotEnoughTokens(uint8 tokenID);
 
-    constructor () ERC1155(""){
+    uint8 public constant VELAR_ID = 0;
+    //Tokens
+    uint8 public constant PLUTON_ID = 1;
+    uint8 public constant AURORA_ID = 2;
+    uint8 public constant NEXOS_ID = 3;
+    //Assets
+    uint8 public constant IMPERIAL_APEX_ID = 4;
+    uint8 public constant CITADEL_ID = 5;
+    uint8 public constant GRANDEUR_ID = 6;
+    uint8 public constant FORTRESS_ID = 7;
+    uint8 public constant CASTLE_ID = 8;
+    uint8 public constant STRONGHOLD_ID = 9;
+    uint8 public constant BASTION_ID = 10;
+    //COSTS
+    uint32 public constant IMPERIAL_APEX_COST = 100000;
+    uint32 public constant CITADEL_COST = 75000;
+    uint32 public constant GRANDEUR_COST = 50000;
+    uint16 public constant FORTRESS_COST = 30000;
+    uint16 public constant CASTLE_COST = 20000;
+    uint16 public constant STRONGHOLD_COST = 10000;
+    uint16 public constant BASTION_COST = 7500;
+    uint8 public constant PLUTON_COST = 10;
+    uint8 public constant AURORA_COST = 5;
+    uint8 public constant NEXOS_COST = 2;
 
+    mapping(uint8 => uint32) public costs;
+
+    constructor(
+        string memory IMPERIAL_APEX_URI,
+        string memory CITADEL_URI,
+        string memory GRANDEUR_URI,
+        string memory FORTRESS_URI,
+        string memory CASTLE_URI,
+        string memory STRONGHOLD_URI,
+        string memory BASTION_URI
+    ) ERC1155("") {
+        _setURI(IMPERIAL_APEX_ID, IMPERIAL_APEX_URI);
+        _setURI(CITADEL_ID, CITADEL_URI);
+        _setURI(GRANDEUR_ID, GRANDEUR_URI);
+        _setURI(FORTRESS_ID, FORTRESS_URI);
+        _setURI(CASTLE_ID, CASTLE_URI);
+        _setURI(STRONGHOLD_ID, STRONGHOLD_URI);
+        _setURI(BASTION_ID, BASTION_URI);
+        costs[IMPERIAL_APEX_ID] = IMPERIAL_APEX_COST;
+        costs[CITADEL_ID] = CITADEL_COST;
+        costs[GRANDEUR_ID] = GRANDEUR_COST;
+        costs[FORTRESS_ID] = FORTRESS_COST;
+        costs[CASTLE_ID] = CASTLE_COST;
+        costs[STRONGHOLD_ID] = STRONGHOLD_COST;
+        costs[BASTION_ID] = BASTION_COST;
+        costs[PLUTON_ID] = PLUTON_COST;
+        costs[AURORA_ID] = AURORA_COST;
+        costs[NEXOS_ID] = NEXOS_COST;
+    }
+
+    function fundVelar() public {
+        _mint(msg.sender, VELAR_ID, 100, "");
+    }
+
+    function mintTokens( uint8 _tokenID,uint _amount) public{
+        if(balanceOf(msg.sender, VELAR_ID)  < _amount * costs[_tokenID]) {
+            revert Assets_NotEnoughTokens(_tokenID);
+        }
+        _safeTransferFrom(msg.sender, address(this), VELAR_ID, _amount * costs[_tokenID], "");
+        _mint(msg.sender, _tokenID, _amount, "");
+    }
+
+    function sendTokens(address _to, uint _amount, uint8 _tokenId) public{
+        _safeTransferFrom(msg.sender, _to, _tokenId, _amount, "");
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC1155, ERC1155Holder)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
---
+++ test/AssetsTest.t.sol
@@ -0,0 +1,107 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Assets} from "../src/Assets.sol";
+import {Test,console} from "forge-std/Test.sol";
+
+contract AssetsTest is Test {
+    uint8 public constant VELAR_ID = 0;
+    //Tokens
+    uint8 public constant PLUTON_ID = 1;
+    uint8 public constant AURORA_ID = 2;
+    uint8 public constant NEXOS_ID = 3;
+    //Assets
+    uint8 public constant IMPERIAL_APEX_ID = 4;
+    uint8 public constant CITADEL_ID = 5;
+    uint8 public constant GRANDEUR_ID = 6;
+    uint8 public constant FORTRESS_ID = 7;
+    uint8 public constant CASTLE_ID = 8;
+    uint8 public constant STRONGHOLD_ID = 9;
+    uint8 public constant BASTION_ID = 10;
+    //COSTS
+    uint32 public constant IMPERIAL_APEX_COST = 100000;
+    uint32 public constant CITADEL_COST = 75000;
+    uint32 public constant GRANDEUR_COST = 50000;
+    uint16 public constant FORTRESS_COST = 30000;
+    uint16 public constant CASTLE_COST = 20000;
+    uint16 public constant STRONGHOLD_COST = 10000;
+    uint16 public constant BASTION_COST = 7500;
+    uint8 public constant PLUTON_COST = 10;
+    uint8 public constant AURORA_COST = 5;
+    uint8 public constant NEXOS_COST = 2;
+    uint8 public constant VELAR_FUND_AMOUNT = 100;
+    Assets assets;
+    address USER = address(0x1);
+    function setUp() public{
+        assets = new Assets(
+            "Imperial_Apex",
+            "Citadel",
+            "Grandeur",
+            "Fortress",
+            "Castle",
+            "Stronghold",
+            "Bastion"
+        );
+
+    }
+
+    function testURIs() public {
+        assertEq(assets.uri(IMPERIAL_APEX_ID), "Imperial_Apex");
+        assertEq(assets.uri(CITADEL_ID), "Citadel");
+        assertEq(assets.uri(GRANDEUR_ID), "Grandeur");
+        assertEq(assets.uri(FORTRESS_ID), "Fortress");
+        assertEq(assets.uri(CASTLE_ID), "Castle");
+        assertEq(assets.uri(STRONGHOLD_ID), "Stronghold");
+        assertEq(assets.uri(BASTION_ID), "Bastion");
+    }
+
+    function testFundVelar() public {
+        vm.prank(USER);
+        assets.fundVelar();
+        assertEq(assets.balanceOf(USER, VELAR_ID), VELAR_FUND_AMOUNT);
+    }
+
+    function testFundTokensSendsTokensToUser() public {
+        uint8 fundAmount = 20;
+        vm.startPrank(USER);
+        assets.fundVelar();
+        assets.mintTokens(NEXOS_ID, fundAmount);
+        assertEq(assets.balanceOf(USER, NEXOS_ID), fundAmount);
+        vm.stopPrank();
+    }
+
+    function testFundTokensSendsVelarsToTheContract() public {
+        uint8 fundAmount = 20;
+        vm.startPrank(USER);
+        assets.fundVelar();
+        assets.mintTokens(NEXOS_ID, fundAmount);
+        assertEq(assets.balanceOf(address(assets), VELAR_ID), fundAmount * NEXOS_COST);
+        vm.stopPrank();
+    }
+
+    function testFundTokensDeductsVelarsFromUser() public {
+        uint8 fundAmount = 20;
+        vm.startPrank(USER);
+        assets.fundVelar();
+        assets.mintTokens(NEXOS_ID, fundAmount);
+        assertEq(assets.balanceOf(USER, VELAR_ID), VELAR_FUND_AMOUNT - (fundAmount * NEXOS_COST));
+        vm.stopPrank();
+    }
+
+    function testFundTokensRevertsIfUserDoesNotHaveEnoughVelars() public {
+        uint8 fundAmount = 200;
+        vm.startPrank(USER);
+        assets.fundVelar();
+        vm.expectRevert();
+        assets.mintTokens(NEXOS_ID, fundAmount);
+        vm.stopPrank();
+    }
+
+    function testSendTokensToTheContract() public{
+        vm.startPrank(USER);
+        assets.fundVelar();
+        assets.sendTokens(address(assets), 10, VELAR_ID);
+        assertEq(assets.balanceOf(address(assets), VELAR_ID), 10);
+    }
+
+}
